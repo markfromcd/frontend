02ff074e973eca59388aa44965802b8825a1684a
markfromcd <114243872+markfromcd@users.noreply.github.com>
2025-04-22 14:45:26 -0400

    The first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Product Management Page';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Product } from './product';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'products', component: ProductListComponent},
+    {path: '', redirectTo: 'products', pathMatch: 'full'}, // redirect to products when the app starts with no path
+    {path: 'create-product', component: CreateProductComponent},
+    {path: 'update-product/:id', component: UpdateProductComponent},
+    {path: 'view-product/:id', component: ProductDetailsComponent},
+];

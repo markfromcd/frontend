3e5767e55e20df8ceafd73b4a8b860372ad40c86
markfromcd <114243872+markfromcd@users.noreply.github.com>
2025-04-23 14:50:44 -0400

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { UpdateProductComponent } from './update-product/update-product.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
 export const routes: Routes = [
@@ -12,4 +17,13 @@ export const routes: Routes = [
     {path: 'create-product', component: CreateProductComponent},
     {path: 'update-product/:id', component: UpdateProductComponent},
     {path: 'view-product/:id', component: ProductDetailsComponent},
+
+    // Customer routes
+    {path: 'customers', component: CustomerListComponent},
+    {path: 'create-customer', component: CreateCustomerComponent},
+    {path: 'update-customer/:id', component: UpdateCustomerComponent},
+    {path: 'view-customer/:id', component: CustomerDetailsComponent},
+
+    // Shopping Cart routes
+    {path: 'view-cart/:id', component: ShoppingCartComponent},
 ];
